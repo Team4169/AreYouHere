13bb2b89ac1db428dec54cc42a5950eb2837fe44
block7 <block7@b339-computer-lab-7.local>
2016-04-07 12:50:08 -0400

    Initial Commit

---
+++ AreYouHere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AreYouHere
+//
+//  Created by block7 on 4/7/16.
+//  Copyright © 2016 Jack Doherty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
