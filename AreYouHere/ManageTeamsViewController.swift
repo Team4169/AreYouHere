2e508d83fb7a380afa01b0330dbae35809a64fe8
thatnerdjack <jackjyro@gmail.com>
2016-04-28 20:58:47 -0400

    change from [String] to NSArray

--- AreYouHere/ManageTeamsViewController.swift
+++ AreYouHere/ManageTeamsViewController.swift
@@ -12,15 +12,15 @@ import Firebase
 class ManageTeamsViewController: UIViewController {
     @IBOutlet weak var picker: UIPickerView!
     
-    var pickerData: [String] = [String]()
+    var pickerData: NSArray?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let userRef = Variables.userRef!
-        userRef.observeEventType(.Value, withBlock: { snapshot in
-            if let writeableTeams = snapshot.value.objectForKey("writeableTeams") {
-                self.pickerData = writeableTeams as! [String]
+        userRef.childByAppendingPath("writeableTeams").observeEventType(.Value, withBlock: { snapshot in
+            if let writeableTeams = snapshot.value.objectForKey("frc") {
+                self.pickerData = writeableTeams as! NSArray
                 //NOTE: You changed the file structure so now teams are lists under UID/PROGRAM/teamNumber
             }
         })
