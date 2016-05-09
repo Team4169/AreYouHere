b3668114d535f2359d30ad0a3ac15971daec82a4
block7 <block7@b339lab31.local>
2016-05-09 13:40:18 -0400

    working on creating teams and associating team member accounts to team

---
+++ AreYouHere/CreateTeamViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CreateTeamViewController.swift
+//  AreYouHere
+//
+//  Created by block7 on 5/9/16.
+//  Copyright © 2016 Jack Doherty. All rights reserved.
+//
+
+import UIKit
+
+class CreateTeamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
